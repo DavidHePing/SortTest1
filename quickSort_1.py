c9a51d5485bac05e3249f2d1900e90fecc7011ae
Dav He <s991179@gmail.com>
2024-10-14 23:41:17 +0800

    feat: quick sort test 1

---
+++ quickSort_1.py
@@ -0,0 +1,13 @@
+l = [0, 6, 73, 18, 84, 81, 57, 77, 83, 78, 71, 67, 36, 93, 93, 33, 70, 100, 84, 64]
+
+def quickSort(arr: list) -> list:
+    if not arr:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quickSort(left) + middle + quickSort(right)
+
+print(quickSort(l))
