beaf0c0c4d459c186063005ae6009f0459681398
Dav He <s991179@gmail.com>
2024-10-24 00:54:46 +0800

    fix: fix bug of start

--- quickSort_2.py
+++ quickSort_2.py
@@ -1,20 +1,14 @@
-l1 = [0, 6, 73, 18, 84, 81, 57, 77, 83, 78, 71, 67, 36, 93, 93, 33, 70, 100, 84, 64]
-l2 = [9, 4, 1, 6, 7, 3, 8, 2, 5]
-l3 = [3, 8, 2, 5, 1, 4, 7, 6]
-l4 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-l5 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-
 def quickSort(arr: list, start: int, end: int):
     if start >= end:
-        return 0
+        return
 
     mid = start + (end - start) // 2
     arr[mid], arr[end] = arr[end], arr[mid]
     pivot = arr[end]
     
-    left = 0
+    left = start
     
-    for right in range(end):
+    for right in range(start, end):
         if arr[right] <= pivot:
             arr[left], arr[right] = arr[right], arr[left]
             left += 1
@@ -22,9 +16,15 @@ def quickSort(arr: list, start: int, end: int):
     arr[left], arr[end] = arr[end], arr[left]
 
     new_pivot = left
-    quickSort(arr, 0, new_pivot - 1)
+    quickSort(arr, start, new_pivot - 1)
     quickSort(arr, new_pivot + 1, end)
 
+# Testing the function with your lists
+l1 = [0, 6, 73, 18, 84, 81, 57, 77, 83, 78, 71, 67, 36, 93, 93, 33, 70, 100, 84, 64]
+l2 = [9, 4, 1, 6, 7, 3, 8, 2, 5]
+l3 = [3, 8, 2, 5, 1, 4, 7, 6]
+l4 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+l5 = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 quickSort(l1, 0, len(l1) - 1)
 print(l1)
